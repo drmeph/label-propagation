5559bdc87e9917057e325c6b5747cf78bf36dc78
Kevin Dorfer <kevin.dorfer@ethoca.com>
2021-03-09 17:34:48 -0500

    added semi supervised learning and supervised learning using propagated labels.

--- label-propagation.py
+++ label-propagation.py
@@ -2,6 +2,8 @@ from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+from sklearn.semi_supervised import LabelPropagation
+from numpy import concatenate
 
 print("Starting Dataset Creation!")
 
@@ -21,11 +23,47 @@ print('Unlabeled Train Set:', X_test_unlab.shape, y_test_unlab.shape)
 print('Test Set:', X_test.shape, y_test.shape)
 
 
+# Supervised Learning first #
+
 # define model
 model = LogisticRegression()
 # fit model on labeled dataset
 model.fit(X_train_lab, y_train_lab)
 
+# make predictions on hold out test set
+yhat = model.predict(X_test)
+# calculate score for test set
+score = accuracy_score(y_test, yhat)
+# summarize score
+print('Supervised Learning Accuracy: %.3f' % (score*100))
+
+
+# Semi Supervised Learning Then #
+
+# create the training dataset input
+X_train_mixed = concatenate((X_train_lab, X_test_unlab))
+# create "no label" for unlabeled data
+nolabel = [-1 for _ in range(len(y_test_unlab))]
+# recombine training dataset labels
+y_train_mixed = concatenate((y_train_lab, nolabel))
+# define model
+model2 = LabelPropagation()
+# fit model on training dataset
+model2.fit(X_train_mixed, y_train_mixed)
+# make predictions on hold out test set
+yhat = model2.predict(X_test)
+# calculate score for test set
+score = accuracy_score(y_test, yhat)
+# summarize score
+print('Semi-Supervised Learning Accuracy: %.3f' % (score*100))
+
+
+# Supervised Learning using propagated labels finally #
+
+# get labels for entire training dataset data
+tran_labels = model2.transduction_
+# fit supervised learning model on entire training dataset
+model.fit(X_train_mixed, tran_labels)
 # make predictions on hold out test set
 yhat = model.predict(X_test)
 # calculate score for test set
